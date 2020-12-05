33976dd4e851dbdb22451ebbeff7fb6adb98c450
Luca Amoriello <luca.amoriello@hotmail.it>
2020-12-05 16:21:40 +0100

    Use cookies instead of object

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,33 +2,43 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { webUserModel } from '../classes/webUserModel';
 import { globalVariables } from './globalVariables.service';
-
+import { CookieService } from 'ngx-cookie-service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmIyNWQ2YWJjNTg5YWE5NDgyM2FkODgiLCJpYXQiOjE2MDU1NTE4MjF9.L4ahXWdoD_5JJVS3Yic_7uiLrr0KT19GIqXE77pMobo
-constructor(private globalVariables: globalVariables, private http:HttpClient) { }
+  //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmIyNWQ2YWJjNTg5YWE5NDgyM2FkODgiLCJpYXQiOjE2MDU1NTE4MjF9.L4ahXWdoD_5JJVS3Yic_7uiLrr0KT19GIqXE77pMobo
+  constructor(
+    private globalVariables: globalVariables,
+    private cookieService: CookieService,
+    private http: HttpClient
+  ) {}
 
-async getUserProfile(id: any, user: webUserModel) {
-  let headers = new HttpHeaders();
-  headers = headers.append('auth-token', user.token);
-  //  console.log(this.globalVariables.apiURL+this.globalVariables.apiVersion 
-  // + this.globalVariables.apiUser + id); 
- return  await this.http.get
-     (this.globalVariables.apiURL+this.globalVariables.apiVersion 
-      + this.globalVariables.apiUser + id,{headers: headers}).toPromise();
- }
+  async getUserProfile(id: any): Promise<any> {
+    let headers = new HttpHeaders();
+    headers = headers.append('auth-token', this.cookieService.get('auth-token'));
+    //  console.log(this.globalVariables.apiURL+this.globalVariables.apiVersion
+    // + this.globalVariables.apiUser + id);
+    return await this.http
+      .get(
+        this.globalVariables.apiURL +
+          this.globalVariables.apiVersion +
+          this.globalVariables.apiUser + id,
+        { headers: headers }
+      )
+      .toPromise();
+  }
 
- async addNewUser(data){
-   return await this.http.post(this.globalVariables.apiURL+this.globalVariables.apiVersion 
-    + this.globalVariables.apiUser + 'register', data, {observe : 'response'}).toPromise();
-
-    
- }
-
- 
-   
-
-
+  async addNewUser(data) {
+    return await this.http
+      .post(
+        this.globalVariables.apiURL +
+          this.globalVariables.apiVersion +
+          this.globalVariables.apiUser +
+          'register',
+        data,
+        { observe: 'response' }
+      )
+      .toPromise();
+  }
 }
