707b56dbbf434e602b560de70754055683e9a26f
Luca Amoriello <luca.amoriello@hotmail.it>
2020-12-05 16:21:15 +0100

    Format doc

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { webUserModel } from '../classes/webUserModel';
-import { globalVariables } from '../services/globalVariables.service';
 import { LoginService } from '../services/login.service';
 import { UserService } from '../services/user.service';
 import { webUserNgService } from '../services/userNg.service';
@@ -11,79 +10,102 @@ import { webUserNgService } from '../services/userNg.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public emailError: boolean;
+  public passwordError: boolean;
 
-  public emailError: boolean
-  public passwordError: boolean
-
-
-  constructor(private fb: FormBuilder, 
-    private loginService: LoginService, 
-    private webUserNgService : webUserNgService, 
+  constructor(
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private webUserNgService: webUserNgService,
     private cookieService: CookieService,
     private userService: UserService,
-    private _router: Router) { }
+    private _router: Router
+  ) {}
 
   loginForm = this.fb.group({
-    email: ['', [
-      Validators.required,
-      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      password: ['', [Validators.required]],
-  })
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ],
+    ],
+    password: ['', [Validators.required]],
+  });
 
   ngOnInit(): void {
     // this.userService.getUserProfile("luca.amoriello@hotmail.it").then(result =>{
     //     console.log(result);
     // });
 
-          // Instead of removing everything we should retrive latest information about the user and set them into cookies/localstorage
-        localStorage.removeItem('revealCards');
-        localStorage.removeItem('cardSelected');
-        localStorage.removeItem('cardAdded');
-        localStorage.removeItem('match');
-        localStorage.removeItem('cardsOnTable');
-        localStorage.removeItem('cardsInHand');
+    // Instead of removing everything we should retrive latest information about the user and set them into cookies/localstorage
+    localStorage.removeItem('revealCards');
+    localStorage.removeItem('cardSelected');
+    localStorage.removeItem('cardAdded');
+    localStorage.removeItem('match');
+    localStorage.removeItem('cardsOnTable');
+    localStorage.removeItem('cardsInHand');
 
-        if(this.cookieService.get('user-id') !=undefined && this.cookieService.get('user-id')!=null && this.cookieService.get('user-id')!='' && localStorage.getItem('user') != undefined && localStorage.getItem('user') != '' ){
-
-          localStorage.setItem('user', JSON.stringify(this.userService.getUserProfile(this.cookieService.get('user-id'), JSON.parse(localStorage.getItem('user')))))
-          this._router.navigateByUrl('/startgame');
-          return
-        }
-
+    if (
+      this.cookieService.get('user-id') != undefined &&
+      this.cookieService.get('user-id') != null &&
+      this.cookieService.get('user-id') != '' &&
+      localStorage.getItem('user') != undefined &&
+      localStorage.getItem('user') != ''
+    ) {
+      localStorage.setItem(
+        'user',
+        JSON.stringify(
+          this.userService.getUserProfile(
+            this.cookieService.get('user-id')
+          )
+        )
+      );
+      this._router.navigateByUrl('/startgame');
+      return;
+    }
   }
 
   sendLogin() {
     let _ = this;
-    this.emailError = false
-    this.passwordError = false
+    this.emailError = false;
+    this.passwordError = false;
 
-    _.loginService.sendLogin(this.loginForm.value).then(res =>{
-      if(res.status == 200){
-        var webUser = new webUserModel();
-      webUser.setWebUser(res.body['user'], res.body['token']);
-      localStorage.setItem('user', JSON.stringify(webUser));
-      _.webUserNgService.changeWebUser(webUser);
-      this.cookieService.set('auth-token', webUser.token);
-      this.cookieService.set('user-id', webUser._id);
-      this._router.navigateByUrl('/startgame');
-      } 
-    }).catch(err => {
-      if(err.status == 400 && (err.error == "EMAIL or password is wrong" || err.error == '"email" must be a valid email')){
-        this.emailError = true
-        setTimeout(function () {
-          _.emailError = false
-        }, 4000);
-      } else if(err.status == 400 && err.error  == "Email or PASSWORD is wrong"){
-        this.passwordError = true
-        setTimeout(function () {
-          _.passwordError = false
-        }, 4000);
-      }
-    });
-    
+    _.loginService
+      .sendLogin(this.loginForm.value)
+      .then((res) => {
+        if (res.status == 200) {
+          var webUser = new webUserModel();
+          webUser.setWebUser(res.body['user'], res.body['token']);
+          localStorage.setItem('user', JSON.stringify(webUser));
+          _.webUserNgService.changeWebUser(webUser);
+          this.cookieService.set('auth-token', webUser.token);
+          this.cookieService.set('user-id', webUser._id);
+          this._router.navigateByUrl('/startgame');
+        }
+      })
+      .catch((err) => {
+        if (
+          err.status == 400 &&
+          (err.error == 'EMAIL or password is wrong' ||
+            err.error == '"email" must be a valid email')
+        ) {
+          this.emailError = true;
+          setTimeout(function () {
+            _.emailError = false;
+          }, 4000);
+        } else if (
+          err.status == 400 &&
+          err.error == 'Email or PASSWORD is wrong'
+        ) {
+          this.passwordError = true;
+          setTimeout(function () {
+            _.passwordError = false;
+          }, 4000);
+        }
+      });
   }
-
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,68 +7,97 @@ import { webUserNgService } from '../services/userNg.service';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  public emailError: boolean;
+  public genericError: boolean;
+  public usernameError: boolean;
+  public registrationSuccess: boolean;
 
-
-  public emailError: boolean
-  public genericError: boolean
-  public usernameError: boolean
-  public registrationSuccess: boolean
-
-  constructor(private fb: FormBuilder,
-    private userService : UserService,
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
     private router: Router,
-    private webUserNgService : webUserNgService) { }
+    private webUserNgService: webUserNgService
+  ) {}
 
   registrationForm = this.fb.group({
-    name: ['' , [Validators.required, Validators.minLength(2), Validators.maxLength(255)]],
-    surname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]],
-    email: ['', [
-      Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      
-    username: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(255)]],
-    password: ['', [Validators.required, Validators.pattern(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,30}$/) ]],
-  })
+    name: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(255)],
+    ],
+    surname: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(255)],
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ],
+    ],
+
+    username: [
+      '',
+      [Validators.required, Validators.minLength(4), Validators.maxLength(255)],
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,30}$/
+        ),
+      ],
+    ],
+  });
 
   ngOnInit(): void {
-    this.webUserNgService.webUsershared.subscribe(message=>{
+    this.webUserNgService.webUsershared.subscribe((message) => {
       console.log(message);
-    })
+    });
   }
 
-  createUser(){
-    let _ = this
+  createUser() {
+    let _ = this;
 
-    _.userService.addNewUser(this.registrationForm.value).then(res =>{
-
-      if(res.status == 200){
-        this.registrationSuccess = true
-        setTimeout(function () {
-          _.router.navigate(['/'])
-        }, 6000);
-
-      }
-    }).catch(error => {
-      console.log(error)
-      if (error.status == 400 && (error.error == "Email already exists" || error.error == '"email" must be a valid email')){
-        this.emailError = true
-        setTimeout(function () {
-          _.emailError = false
-        }, 4000);
-      }else if(error.status == 400 && error.error == "Username already exists"){
-        this.usernameError = true
-        setTimeout(function () {
-          _.usernameError = false
-        }, 4000);
-      }else{
-        this.genericError = true
-        setTimeout(function () {
-          _.genericError = false
-        }, 4000);
-      }
-    })
+    _.userService
+      .addNewUser(this.registrationForm.value)
+      .then((res) => {
+        if (res.status == 200) {
+          this.registrationSuccess = true;
+          setTimeout(function () {
+            _.router.navigate(['/']);
+          }, 6000);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if (
+          error.status == 400 &&
+          (error.error == 'Email already exists' ||
+            error.error == '"email" must be a valid email')
+        ) {
+          this.emailError = true;
+          setTimeout(function () {
+            _.emailError = false;
+          }, 4000);
+        } else if (
+          error.status == 400 &&
+          error.error == 'Username already exists'
+        ) {
+          this.usernameError = true;
+          setTimeout(function () {
+            _.usernameError = false;
+          }, 4000);
+        } else {
+          this.genericError = true;
+          setTimeout(function () {
+            _.genericError = false;
+          }, 4000);
+        }
+      });
   }
-
 }
